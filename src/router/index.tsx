78049f339cb5743918c89f5556148ffa7ca36624
MoonJuhan <codemike@naver.com>
2024-03-27 21:34:06 +0900

    feat: Add layout

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { lazy } from 'react'
+import AppLayout from '../components/app/AppLayout'
 
 const ViewItems = lazy(() => import('../components/views/items/ViewItems'))
 const ViewCart = lazy(() => import('../components/views/cart/ViewCart'))
@@ -7,11 +8,17 @@ const ViewCart = lazy(() => import('../components/views/cart/ViewCart'))
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <ViewItems />,
-  },
-  {
-    path: '/cart',
-    element: <ViewCart />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <ViewItems />,
+      },
+      {
+        path: '/cart',
+        element: <ViewCart />,
+      },
+    ],
   },
 ])
 
