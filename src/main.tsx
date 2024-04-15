e2462e373f4b51d6b571efce564d8856ac2f9ab3
MoonJuhan <codemike@naver.com>
2024-04-15 19:12:11 +0900

    feat: Add App.tsx and Recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom/client'
+import { RecoilRoot } from 'recoil'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
 import './index.css'
+import App from './App'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
-  </QueryClientProvider>,
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </RecoilRoot>,
 )
